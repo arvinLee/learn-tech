3a1a63d03d3f0c12f694824e2fbe84f413fe0217
arvinLee <liyawen1989@gmail.com>
2017-06-04 21:50:29 +0800

    init project

---
+++ learn-dal/src/main/java/org/awa/tech/learn/dao/App.java
@@ -0,0 +1,13 @@
+package org.awa.tech.learn.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ learn-middleware/src/main/java/org/awa/tech/learn/middleware/App.java
@@ -0,0 +1,13 @@
+package org.awa.tech.learn.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
